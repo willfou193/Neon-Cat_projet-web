73974bf87aa065861e8dddcfd5ca8b5c3a0f6047
William Foucher <e1938665@cmaisonneuve.qc.ca>
2022-10-20 01:45:03 -0400

    programmation page des profs début

--- page.php
+++ page.php
@@ -13,8 +13,47 @@ get_header();
 <main class="site__main">
     <?php if(have_posts()):the_post(); ?>
     <h1><?php the_title(); ?></h1>
-    
-    <?php the_content(); ?>
+        <?php
+           global $wp;
+           $current_slug = add_query_arg( array(), $wp->request );
+        ?>
+            <?php if($current_slug == 'les-profs'): ?>
+                <div class="contenant ">
+                    <input type="radio" name="position" checked />
+                    <input type="radio" name="position" />
+                    <input type="radio" name="position" />
+                    <input type="radio" name="position" />
+                    <input type="radio" name="position" />
+                    <input type="radio" name="position" />
+                    <input type="radio" name="position" />
+                    <input type="radio" name="position" />
+                    <input type="radio" name="position" />
+                    <input type="radio" name="position" />
+                    <input type="radio" name="position" />
+                    <input type="radio" name="position" />
+                    <input type="radio" name="position" />
+                    <input type="radio" name="position" />
+            
+                    <div class="carousel">
+                        <div class="item"></div>
+                        <div class="item"></div>
+                        <div class="item"></div>
+                        <div class="item"></div>
+                        <div class="item"></div>
+                        <div class="item"></div>
+                        <div class="item"></div>
+                        <div class="item"></div>
+                        <div class="item"></div>
+                        <div class="item"></div>
+                        <div class="item"></div>
+                        <div class="item"></div>
+                        <div class="item"></div>
+                        <div class="item"></div>
+                    <div>
+                </div>
+            <?php endif; ?>
+                
+     
     <?php endif; ?>
 </main>
 <?php get_footer(); ?>
