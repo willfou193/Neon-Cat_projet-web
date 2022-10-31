385fe4fd120c4f0af1d3565d7f28ea2999a79e61
William Foucher <e1938665@cmaisonneuve.qc.ca>
2022-10-30 20:49:43 -0400

    correction page.php potentiel bug avec siteground

--- cours.php
+++ cours.php
@@ -1,26 +1,6 @@
 <?php get_header(); ?>
 <main id="main__cours">
-    <p>Page de cours</p>
-    <div id="contenant carrousel-cours-contenant"><!-- Debut carrousel -->
-        <input type="radio" name="position" checked />
-        <input type="radio" name="position" />
-        <input type="radio" name="position" />
-        <input type="radio" name="position" />
-        <input type="radio" name="position" />
-        <input type="radio" name="position" />
-        <input type="radio" name="position" />
-        <!-- Ajout d'une input -->
-        <div id="carousel carrousel-cours">
-            <div class="item"></div>
-            <div class="item"></div>
-            <div class="item"></div>
-            <div class="item"></div>
-            <div class="item"></div>
-            <div class="item"></div>
-            <div class="item"></div>
-            <!-- Ajout d'une div -->
-        <div>
-
+    
 
 </main>
 <?php get_footer(); ?>
