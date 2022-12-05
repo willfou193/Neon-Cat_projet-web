c297e8b8c86fae02a38060d6e59eee270d578bb2
Christopher Stevens <91079060+Gcarburant@users.noreply.github.com>
2022-12-05 14:16:49 -0500

    ajout des opportunités

---
+++ category-opportunite.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package _underscore
+ */
+
+get_header();
+?>
+<main id="primary" class="site-main" >
+
+    
+
+        <div class="grid-container">
+            <div class="lesOpportunites"></div>
+                <div class="opportunite3">
+                    <img src="media/images/R.jpg" alt="uqat">
+                </div>
+                <div class="opportunite2"></div>
+                <div class="opportunite1"></div>
+        </div>
+
+               
+
+
+</main>
+
+<?php
+    get_footer();
+?>
